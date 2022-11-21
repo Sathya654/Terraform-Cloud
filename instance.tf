14ebf99966d6c1970eef8fd6602debb4d8d3dce1
Sathya654 <115213543+Sathya654@users.noreply.github.com>
2022-11-21 10:45:27 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 resource "aws_instance" "web" {
   ami           = "ami-02dc8ad50da58fffd"
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Hopp1"
